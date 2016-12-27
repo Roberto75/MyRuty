a1236b3b8a9f7574275e1832b899305c62f98aad
Daitarn <roberto.rutigliano@gmail.com>
2016-12-27 20:28:51 +0100

    jpg

--- MyRuty2.0/FormRawRemoveJPG.Designer.cs
+++ MyRuty2.0/FormRawRemoveJPG.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblFolder = new System.Windows.Forms.Label();
+            this.chkTopDirectoryOnly = new System.Windows.Forms.CheckBox();
             this.btnFolder = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -46,6 +49,9 @@
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.lblCount);
+            this.splitContainer1.Panel1.Controls.Add(this.lblFolder);
+            this.splitContainer1.Panel1.Controls.Add(this.chkTopDirectoryOnly);
             this.splitContainer1.Panel1.Controls.Add(this.btnFolder);
             // 
             // splitContainer1.Panel2
@@ -55,13 +61,41 @@
             this.splitContainer1.SplitterDistance = 165;
             this.splitContainer1.TabIndex = 0;
             // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(3, 142);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(35, 13);
+            this.lblCount.TabIndex = 3;
+            this.lblCount.Text = "label1";
+            // 
+            // lblFolder
+            // 
+            this.lblFolder.AutoSize = true;
+            this.lblFolder.Location = new System.Drawing.Point(146, 36);
+            this.lblFolder.Name = "lblFolder";
+            this.lblFolder.Size = new System.Drawing.Size(35, 13);
+            this.lblFolder.TabIndex = 2;
+            this.lblFolder.Text = "label1";
+            // 
+            // chkTopDirectoryOnly
+            // 
+            this.chkTopDirectoryOnly.AutoSize = true;
+            this.chkTopDirectoryOnly.Location = new System.Drawing.Point(35, 70);
+            this.chkTopDirectoryOnly.Name = "chkTopDirectoryOnly";
+            this.chkTopDirectoryOnly.Size = new System.Drawing.Size(114, 17);
+            this.chkTopDirectoryOnly.TabIndex = 1;
+            this.chkTopDirectoryOnly.Text = "Top Direcrory Only";
+            this.chkTopDirectoryOnly.UseVisualStyleBackColor = true;
+            // 
             // btnFolder
             // 
-            this.btnFolder.Location = new System.Drawing.Point(35, 28);
+            this.btnFolder.Location = new System.Drawing.Point(35, 29);
             this.btnFolder.Name = "btnFolder";
             this.btnFolder.Size = new System.Drawing.Size(79, 26);
             this.btnFolder.TabIndex = 0;
-            this.btnFolder.Text = "button1";
+            this.btnFolder.Text = "Folder";
             this.btnFolder.UseVisualStyleBackColor = true;
             this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
             // 
@@ -69,6 +103,8 @@
             // 
             this.listView1.CheckBoxes = true;
             this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
             this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(763, 214);
@@ -85,6 +121,7 @@
             this.Name = "FormRawRemoveJPG";
             this.Text = "FormRawRemoveJPG";
             this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
@@ -97,5 +134,8 @@
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Button btnFolder;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.CheckBox chkTopDirectoryOnly;
+        private System.Windows.Forms.Label lblFolder;
+        private System.Windows.Forms.Label lblCount;
     }
 }
--- MyRuty2.0/FormRawRemoveJPG.cs
+++ MyRuty2.0/FormRawRemoveJPG.cs
@@ -24,21 +24,43 @@ namespace MyApplication
 
         private void init()
         {
-            listView1.Columns.Add("",30);
-            listView1.Columns.Add("Name",250);
-            listView1.Columns.Add("Extension",100);
-            listView1.Columns.Add("Length",200);
-            listView1.Columns.Add("CreationTime",150);
+            // check bosk sulla colonna 0
+            listView1.OwnerDraw = true;
+            listView1.ColumnClick += ListView1_ColumnClick;
+            listView1.DrawColumnHeader += ListView1_DrawColumnHeader;
+            listView1.DrawItem += ListView1_DrawItem;
+            listView1.DrawSubItem += ListView1_DrawSubItem;
+
+
+            listView1.Columns.Add("", 30);
+            listView1.Columns.Add("Folder", 250);
+            listView1.Columns.Add("Name", 250);
+            listView1.Columns.Add("Extension", 100);
+            listView1.Columns.Add("Length", 200);
+            listView1.Columns.Add("CreationTime", 150);
+
+            lblFolder.Text = "";
+            lblCount.Text = "";
         }
+
+
 
         private void btnFolder_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog fd = new FolderBrowserDialog();
-            fd.RootFolder = Environment.SpecialFolder.Desktop;
+            //fd.RootFolder = Environment.SpecialFolder.Recent;
             if (fd.ShowDialog(this) == DialogResult.OK)
             {
                 Debug.WriteLine(fd.SelectedPath);
-                processFolder(new DirectoryInfo(fd.SelectedPath));
+                lblFolder.Text = fd.SelectedPath;
+
+                bool esito = processFolder(new DirectoryInfo(fd.SelectedPath));
+
+                if (esito)
+                {
+                    lblCount.Text = String.Format("Found: {0} ",  listView1.Items.Count);
+                    //listView1.
+                }
             }
         }
 
@@ -49,15 +71,18 @@ namespace MyApplication
             {
                 return false;
             }
-
-
             listView1.Items.Clear();
 
-          
 
             List<FileInfo> toBeDeleted = new List<FileInfo>();
 
-            foreach (FileInfo fi in folder.EnumerateFiles("*.nef", SearchOption.TopDirectoryOnly))
+            SearchOption option = SearchOption.AllDirectories;
+            if (chkTopDirectoryOnly.Checked)
+            {
+                option = SearchOption.TopDirectoryOnly;
+            }
+
+            foreach (FileInfo fi in folder.EnumerateFiles("*.nef", option))
             {
                 //Debug.WriteLine(fi.Name);
 
@@ -68,23 +93,22 @@ namespace MyApplication
 
                 FileInfo fileJpg = new FileInfo(fi.Directory.FullName + Path.DirectorySeparatorChar + temp + ".jpg");
 
-               
+
 
                 if (fileJpg.Exists)
                 {
-                    Debug.WriteLine("To be deleted: " + fileJpg.Name );
+                    Debug.WriteLine("To be deleted: " + fileJpg.Name);
 
 
                     ListViewItem newItem = new ListViewItem();
                     //newItem.Text = fileJpg.Name;
-                    //newItem.Tag = fileJpg;
-
+                    newItem.Tag = fileJpg;
+                    newItem.Checked = false;
+                    newItem.SubItems.Add(fileJpg.DirectoryName);
                     newItem.SubItems.Add(fileJpg.Name);
                     newItem.SubItems.Add(fileJpg.Extension);
                     newItem.SubItems.Add(fileJpg.Length.ToString());
                     newItem.SubItems.Add(fileJpg.CreationTime.ToString());
-
-                    
 
                     listView1.Items.Add(newItem);
 
@@ -96,10 +120,76 @@ namespace MyApplication
 
 
 
-//            listView1.Items = toBeDeleted;
+            //            listView1.Items = toBeDeleted;
 
             return true;
         }
+
+
+
+
+
+
+
+
+        #region "Disegnare la check box sulla colonna 0 "
+        private void ListView1_DrawSubItem(object sender, DrawListViewSubItemEventArgs e)
+        {
+            e.DrawDefault = true;
+        }
+
+        private void ListView1_DrawItem(object sender, DrawListViewItemEventArgs e)
+        {
+            e.DrawDefault = true;
+        }
+
+        private void ListView1_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
+        {
+            if (e.ColumnIndex == 0)
+            {
+                e.DrawBackground();
+                bool value = false;
+                try
+                {
+                    value = Convert.ToBoolean(e.Header.Tag);
+                }
+                catch (Exception)
+                {
+                }
+                CheckBoxRenderer.DrawCheckBox(e.Graphics,
+                    new Point(e.Bounds.Left + 4, e.Bounds.Top + 4),
+                    value ? System.Windows.Forms.VisualStyles.CheckBoxState.CheckedNormal :
+                    System.Windows.Forms.VisualStyles.CheckBoxState.UncheckedNormal);
+            }
+            else
+            {
+                e.DrawDefault = true;
+            }
+        }
+
+        private void ListView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            if (e.Column == 0)
+            {
+                bool value = false;
+                try
+                {
+                    value = Convert.ToBoolean(this.listView1.Columns[e.Column].Tag);
+                }
+                catch (Exception)
+                {
+                }
+                this.listView1.Columns[e.Column].Tag = !value;
+
+                foreach (ListViewItem item in this.listView1.Items)
+                    item.Checked = !value;
+
+                this.listView1.Invalidate();
+            }
+        }
+
+        #endregion
+
 
     }
 }

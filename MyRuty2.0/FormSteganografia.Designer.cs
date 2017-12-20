41f90c91b60a34dde617a13388a4506284159ebf
Daitarn <roberto.rutigliano@gmail.com>
2017-12-20 18:19:41 +0100

    Form Dialog

--- MyRuty2.0/FormSteganografia.Designer.cs
+++ MyRuty2.0/FormSteganografia.Designer.cs
@@ -28,19 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(887, 452);
+            this.splitContainer1.SplitterDistance = 89;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDecode);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(887, 89);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnDecode.Location = new System.Drawing.Point(12, 60);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(75, 23);
+            this.btnDecode.TabIndex = 1;
+            this.btnDecode.Text = "Decodifica";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(780, 60);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(95, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Codifica e salva";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormSteganografia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(887, 452);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "FormSteganografia";
             this.Text = "FormSteganografia";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDecode;
     }
 }
--- MyRuty2.0/FormSteganografia.cs
+++ MyRuty2.0/FormSteganografia.cs
@@ -12,9 +12,61 @@ namespace MyApplication
 {
     public partial class FormSteganografia : Form
     {
+        private Steganografia.PicFactory _factory = new Steganografia.PicFactory();
+        private string _pathFile;
+
         public FormSteganografia()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+           if (! openFileDialog("Immagine (*.bmp;*.tif;*.tiff;*.png)|*.bmp;*.tif;*.tiff;*.png;")) {
+                return;
+            }
+        }
+
+
+        private bool openFileDialog(String filter)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select image";
+            dialog.Filter = filter;
+            dialog.Multiselect = false;
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                _pathFile = dialog.FileName;
+                return true;
+            }
+
+            /*
+            Dim OpenFileDialog1 As New Windows.Forms.OpenFileDialog
+        OpenFileDialog1.Title = "Select image"
+        OpenFileDialog1.Filter = filter
+        OpenFileDialog1.Multiselect = False
+        'OpenFileDialog1.InitialDirectory = Application.ExecutablePath
+        'OpenFileDialog1.FileName = "source.xls"
+
+        If(OpenFileDialog1.ShowDialog(tabPageMain) = Windows.Forms.DialogResult.OK) Then
+           _pathFile = OpenFileDialog1.FileName
+            _addTitleToHeader("File:", _pathFile)
+            Application.DoEvents()
+            Return True
+        End If
+
+        Return False*/
+
+            return false;
+        }
+
+
+
+
     }
 }

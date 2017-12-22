c971482515052e1d780d2135b31b6e0d2f75c490
Daitarn <roberto.rutigliano@gmail.com>
2017-12-22 18:41:16 +0100

    Decode

--- MyRuty2.0/FormSteganografia.cs
+++ MyRuty2.0/FormSteganografia.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,7 @@ namespace MyApplication
     {
         private Steganografia.PicFactory _factory = new Steganografia.PicFactory();
         private string _pathFile;
+        private string _password;
 
         public FormSteganografia()
         {
@@ -27,7 +29,8 @@ namespace MyApplication
 
         private void btnDecode_Click(object sender, EventArgs e)
         {
-           if (! openFileDialog("Immagine (*.bmp;*.tif;*.tiff;*.png)|*.bmp;*.tif;*.tiff;*.png;")) {
+            if (!openFileDialog("Immagine (*.bmp;*.tif;*.tiff;*.png)|*.bmp;*.tif;*.tiff;*.png;"))
+            {
                 return;
             }
         }
@@ -42,8 +45,11 @@ namespace MyApplication
             if (dialog.ShowDialog(this) == DialogResult.OK)
             {
                 _pathFile = dialog.FileName;
+                decode();
                 return true;
             }
+
+
 
             /*
             Dim OpenFileDialog1 As New Windows.Forms.OpenFileDialog
@@ -66,6 +72,64 @@ namespace MyApplication
         }
 
 
+        private void decode()
+        {
+            Steganografia.IDecoder decoder;
+            decoder = _factory.MakeDecoder();
+
+            Steganografia.Stego stego;
+            stego = new Steganografia.Stego(decoder);
+
+            _password = "angylilli";
+
+            try
+            {
+                stego.Decode(_pathFile, _password);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            System.IO.Stream message;
+            message = stego.DecodedFile;
+
+            long length;
+            length = message.Length;
+
+            System.IO.StreamReader sr;
+            sr = new System.IO.StreamReader(message, UnicodeEncoding.UTF32);
+
+            int r;
+            StringBuilder output;
+
+            try
+            {
+                output = new System.Text.StringBuilder((int)length, (int)length);
+                r = sr.Read();
+
+                while (r != -1)
+                {
+                    //output.Append(Chr(r));
+                    output.Append((char)r);
+                    r = sr.Read();
+                }
+
+                Debug.WriteLine(output.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //Return False
+            }
+            finally
+            {
+                sr.Close();
+                stego.Dispose();
+            }
+
+        }
 
 
     }
